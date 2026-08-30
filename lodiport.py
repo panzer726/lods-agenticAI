2ca75a338f845507a9e6eae861b553b7d03677ca
panzer726 <campuuus7@gmail.com>
2026-08-30 13:24:23 +0800

    fix: reuse requests.Session() and use 127.0.0.1 instead of localhost to cut connection latency

--- lodiport.py
+++ lodiport.py
@@ -1,10 +1,12 @@
 import requests
 import json
+session = requests.Session()
+import time
 
 while True:
     user_msg = input("YOU: ")
-
-    resp = requests.post("http://localhost:8000/message",json={"source":"terminal","content":user_msg})
-
+    t1 = time.perf_counter()
+    resp = session.post("http://127.0.0.1:8000/message",json={"source":"terminal","content":user_msg})
+    print(time.perf_counter()-t1)
     print(resp.json()["reply"])
 
